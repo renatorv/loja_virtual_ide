bb5347a67b86c3ad60cfa5a4fe685ffd3d7b86d2
renatorv <renatorv@gmail.com>
2020-02-05 22:23:34 -0300

    Categorias

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:loja_virtual_ide/tabs/home_tab.dart';
+import 'package:loja_virtual_ide/tabs/products_tab.dart';
 import 'package:loja_virtual_ide/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -17,7 +18,14 @@ class HomeScreen extends StatelessWidget {
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
         ),
-        Container(color: Colors.red),
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Produtos'),
+            centerTitle: true,            
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
         Container(color: Colors.green),
         Container(color: Colors.blue),
       ],
